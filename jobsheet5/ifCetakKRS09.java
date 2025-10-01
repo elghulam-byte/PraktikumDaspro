15244d92254cb6a25262d6765649ae7f70a2a972
elghulam-byte <ghulamelfirdausy290307@gmail.com>
2025-10-01 12:16:04 +0700

    modifikasi percobaan 1

--- jobsheet5/ifCetakKRS09.java
+++ jobsheet5/ifCetakKRS09.java
@@ -9,6 +9,8 @@ public class ifCetakKRS09 {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        } else {
+        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
         sc.close();
     }
